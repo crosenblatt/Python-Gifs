44dbaca55ad1b7978356c4d30903fac0e668b1e4
Chris Rosenblatt <rosench13@gmail.com>
2017-12-04 22:27:10 -0500

    add enclosing circle and lines

--- Circles.py
+++ Circles.py
@@ -20,7 +20,14 @@ cir5 = graphics.Circle(ptUpperLeft, 5)
 cir6 = graphics.Circle(ptUpperRight, 5)
 cir7 = graphics.Circle(ptLowerLeft, 5)
 cir8 = graphics.Circle(ptLowerRight, 5)
+cirEnc = graphics.Circle(graphics.Point(250, 250), 50)
 
+lineVert = graphics.Line(graphics.Point(250,200), graphics.Point(250,300))
+lineHoriz = graphics.Line(graphics.Point(200, 250), graphics.Point(300, 250))
+lineDownLeft = graphics.Line(graphics.Point(250 - (25 * math.sqrt(2)), 250 - (25 * math.sqrt(2))), graphics.Point(250 + (25 * math.sqrt(2)), 250 + 25 * (math.sqrt(2))))
+lineUpRight = graphics.Line(graphics.Point(250 - (25 * math.sqrt(2)), 250 + (25 * math.sqrt(2))), graphics.Point(250 + (25 * math.sqrt(2)), 250 - 25 * (math.sqrt(2))))
+
+cirEnc.draw(win)
 cir.draw(win)
 cir2.draw(win)
 cir3.draw(win)
@@ -29,6 +36,10 @@ cir5.draw(win)
 cir6.draw(win)
 cir7.draw(win)
 cir8.draw(win)
+lineVert.draw(win)
+lineHoriz.draw(win)
+lineDownLeft.draw(win)
+lineUpRight.draw(win)
 
 dx = 0.1
 dy = -0.1
@@ -57,6 +68,3 @@ while True:
     cir6.move(-dxDiag, dyDiag)
     cir7.move(dxDiag, -dyDiag)
     cir8.move(-dxDiag, -dyDiag)
-
-
-
